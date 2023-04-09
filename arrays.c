6c5a0d2482ff0c3cb1cfc2094977b20a95a0bc4a
amarja04 <125913981+amarja04@users.noreply.github.com>
2023-04-10 01:49:19 +0530

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+int main(){      
+int i=0;    
+int marks[5];//declaration of array       
+marks[0]=55;//initialization of array    
+marks[1]=60;    
+marks[2]=90;    
+marks[3]=65;    
+marks[4]=75;    
+//traversal of array    
+for(i=0;i<5;i++){      
+printf("%d \n",marks[i]);    
+}//end of for loop     
+return 0;  
+}    
